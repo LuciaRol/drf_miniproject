2d4236aec21fcb2e5a2d77fc4ef27196d1862094
Lucía <luciarol87@gmail.com>
2024-11-03 17:34:47 +0100

    serializers, managemet, update settings

---
+++ blog/management/commands/import_data.py
@@ -0,0 +1,39 @@
+# api/management/commands/import_data.py
+import requests
+from django.core.management.base import BaseCommand
+from api.models import Post, Comment
+
+class Command(BaseCommand):
+    help = "Import data from JSONPlaceholder API"
+
+    def handle(self, *args, **kwargs):
+        # Importar posts
+        posts_response = requests.get("https://jsonplaceholder.typicode.com/posts")
+        posts_data = posts_response.json()
+
+        for post_data in posts_data:
+            Post.objects.update_or_create(
+                id=post_data["id"],
+                defaults={
+                    "user_id": post_data["userId"],
+                    "title": post_data["title"],
+                    "body": post_data["body"],
+                },
+            )
+
+        # Importar comentarios
+        comments_response = requests.get("https://jsonplaceholder.typicode.com/comments")
+        comments_data = comments_response.json()
+
+        for comment_data in comments_data:
+            Comment.objects.update_or_create(
+                id=comment_data["id"],
+                defaults={
+                    "post_id": comment_data["postId"],
+                    "name": comment_data["name"],
+                    "email": comment_data["email"],
+                    "body": comment_data["body"],
+                },
+            )
+
+        self.stdout.write(self.style.SUCCESS("Data imported successfully"))
--- drf_miniproject/settings.py
+++ drf_miniproject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Define the authentication classes
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
